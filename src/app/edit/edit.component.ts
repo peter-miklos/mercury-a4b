782425e821a4cf19fc9d31abfea02d9c224662ac
Peter Miklos <pt.miklos@gmail.com>
2017-03-28 18:10:17 +0100

    refactor edit.component.ts and new.component.html

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,46 +1,46 @@
 import { Component, Input, OnInit }       from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { FormGroup }                      from '@angular/forms';
 import 'rxjs/add/operator/switchMap';
 
 import { SearchService }            from '../_services/search.service';
+import { FormControlService }       from '../_services/form-control.service';
 import { Person }                   from '../_models/person.model';
 import { Address }                  from '../_models/address.model';
 
 @Component({
   selector: 'app-edit',
-  templateUrl: './edit.component.html',
+  templateUrl: '../new/new.component.html',
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
 
-  private person: Person;
-  private address: Address;
+  private person: FormGroup;
   private loading = false;
 
   constructor(
     private searchService: SearchService,
+    private formControlService: FormControlService,
     private activatedRoute: ActivatedRoute,
     private router: Router
   ) { }
 
   ngOnInit() {
+    this.person = this.formControlService.toFormGroup();
     this.activatedRoute.params
         .switchMap((params: Params) => this.searchService.getPerson(+params['id']))
         .subscribe(person => {
-          this.person = person;
-          if (this.person) { this.address = person.address; }
+          this.person = this.formControlService.toFormGroup(person);
         })
   }
 
-  save(): void {
+  submit(model: Person): void {
     this.loading = true;
-    this.person.address = this.address;
-    this.searchService.save(this.person);
+    this.searchService.save(model);
     this.gotoSearch();
   }
 
   gotoSearch(): void {
     this.router.navigate(['/search'])
   }
-
 }
